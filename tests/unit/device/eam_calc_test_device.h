a7d270efe2fa0625864c5cf36de29833183b1dfd
genshen <genshenchu@gmail.com>
2020-05-15 20:14:01 +0800

    test(eam-device): add tests of device side eam calculation: force, elec density, embed energy derivative

--- tests/unit/device/eam_calc_test_device.h
+++ tests/unit/device/eam_calc_test_device.h
@@ -5,6 +5,15 @@
 #ifndef HIP_POT_EAM_CALC_TEST_DEVICE_H
 #define HIP_POT_EAM_CALC_TEST_DEVICE_H
 
+#include "types.h"
+
 void checkPotSplinesCopy(int ele_size, int data_size);
+
+void deviceForce(atom_type::_type_prop_key *key_from, atom_type::_type_prop_key *key_to, double *df_from, double *df_to,
+                 double *dist2, double *forces, size_t len);
+
+void deviceEamChargeDensity(atom_type::_type_prop_key *keys, double *dist2, double *rhos, size_t len);
+
+void deviceEamDEmbedEnergy(atom_type::_type_prop_key *keys, double *rhos, double *dfs, size_t len);
 
 #endif // HIP_POT_EAM_CALC_TEST_DEVICE_H
